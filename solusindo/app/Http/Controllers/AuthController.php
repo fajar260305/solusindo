04e37c9f54481279af26b32386123d03db073142
fajar260305 <100895588+fajar260305@users.noreply.github.com>
2023-01-18 18:15:28 +0700

    Update AuthController.php

--- solusindo/app/Http/Controllers/AuthController.php
+++ solusindo/app/Http/Controllers/AuthController.php
@@ -35,11 +35,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth()->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
+        Auth::logout();
 
         return redirect('/');
     }
